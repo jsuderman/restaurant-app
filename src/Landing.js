1687132f9ece71d14814cac0cb6d2cb596f45832
Justin Suderman <jsuderman1@gmail.com>
2020-09-15 19:24:15 -0400

    added auth page

--- src/Landing.js
+++ src/Landing.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react'
 
 class Landing extends Component {
-    render () {
-        return (
-            <div>
-              <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Menu</a></li>
-        <li><a href="badges.html">Catering</a></li>
-        <li><a href="collapsible.html">Contact</a></li>
-      </ul>
-    </div>
-  </nav>
+  render() {
+    return (
+      <div>
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="sass.html">Menu</a></li>
+              <li><a href="badges.html">Catering</a></li>
+              <li><a href="collapsible.html">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
 
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
-export default Landing 
+export default Landing
 
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react'
+import "./style.css"
 
 class Navbar extends Component {
-    render () {
-        return (
-            <div>
-    <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Menu</a></li>
-        <li><a href="badges.html">Catering</a></li>
-        <li><a href="collapsible.html">Contact</a></li>
-      </ul>
-    </div>
-    </nav>
+  render() {
+    return (
+      <div>
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="sass.html">Menu</a></li>
+              <li><a href="badges.html">Catering</a></li>
+              <li><a href="collapsible.html">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
 
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
-<div id="background" class="view">
-    <div class="full-lg-img">
+{/* <div id="background" class="view">
+  <div class="full-lg-img" >
 
-</div>
+  </div>
 
-</div>
+</div> */}
 
-export default Navbar 
+export default Navbar
 
 
