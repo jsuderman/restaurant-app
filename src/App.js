411a9dc278ba1d313b12e6d683d21e272651a43a
Ben <bsekis21@gmail.com>
2020-09-17 18:45:12 -0400

    debugging

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-import NavbarComponent from "./Components/navbar";
-import HeaderComponent from "./Components/header";
-import DetailComponent from "./Components/detail";
-import IconComponent from "./Components/icon";
-import AboutComponent from "./Components/about";
-import FooterComponent from "./Components/footer";
+import NavbarComponent from "./Components/NavbarComponent";
+import HeaderComponent from "./Components/HeaderComponent";
+import DetailComponent from "./Components/DetailComponent";
+import IconComponent from "./Components/IconComponent";
+import AboutComponent from "./Components/AboutComponent";
+import FooterComponent from "./Components/FooterComponent";
 
 function App() {
-
-  // <Router>
-  // <div className="App">
-  //   <Navbar />
-  //   <Route exact path="/" component={Authentication} />
-  //   <Route exact path="/reservations" component={Reservations} /></div>
   return (
     <div className='App'>
       <NavbarComponent />
