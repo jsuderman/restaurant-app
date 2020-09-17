c707b55ce08054bd2d3e4084c767abe7d896cf5f
Ben <bsekis21@gmail.com>
2020-09-17 13:33:38 -0400

    landingwork

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import Navbar from '../src/Navbar';
-
+import React from "react";
+import NavbarComponent from "./Components/navbar";
+import HeaderComponent from "./Components/header";
+import DetailComponent from "./Components/detail";
+import IconComponent from "./Components/icon";
+import AboutComponent from "./Components/about";
+import FooterComponent from "./Components/footer";
 
 function App() {
   return (
-    <div className="App">
-    <Navbar/>
-      
+    <div className='App'>
+      <NavbarComponent />
+      <HeaderComponent />
+      <DetailComponent />
+      <IconComponent />
+      <AboutComponent />
+      <FooterComponent />
     </div>
   );
 }
+
 export default App;
