46b24f2eee661229357a7f71b09525f7b9c99f00
Kyle Johnson <KJohnson3288@yahoo.com>
2020-10-06 15:47:18 -0400

    session storage changes

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -1,40 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import NavbarComponent from "../components/Navbar";
 import HeaderComponent from "../components/header";
 import AboutComponent from "../components/about";
 import FooterComponent from "../components/footer";
-import "../app.css"
+import "../app.css";
 
 
-const Landing = () => {
+const Landing = (props) => {
+
+    console.log(sessionStorage)
+    
 
     return (
         <div>
             <NavbarComponent/>
             <HeaderComponent/>
-            <h3>Welcome User</h3>
-            <div class="row">
-                <div class="col s12 m6">
-                    <div class="card blue-grey darken-1 current">
-                        <div class="card-content white-text">
-                            <span class="card-title">Current Reservation</span>
+    <h3>Welcome {sessionStorage.getItem("firstName") + " " + sessionStorage.getItem("lastName")}</h3>
+            <div className="row">
+                <div className="col s12 m6">
+                    <div className="card blue-grey darken-1 current">
+                        <div className="card-content white-text">
+                            <span className="card-title">Current Reservation</span>
                             <p>The Last reservation by this user will go here.</p>
                         </div>
-                        <div class="card-action">
+                        <div className="card-action">
 
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col s12 m6">
-                    <div class="card blue-grey darken-1 current">
-                        <div class="card-content white-text">
-                            <span class="card-title">Current Order</span>
+            <div className="row">
+                <div className="col s12 m6">
+                    <div className="card blue-grey darken-1 current">
+                        <div className="card-content white-text">
+                            <span className="card-title">Current Order</span>
                             <p>The last order placed by this user will go here</p>
                         </div>
-                        <div class="card-action">
+                        <div className="card-action">
 
                         </div>
                     </div>
